d2dc959cd833b87d1b1f06adabb27de6db0b6b7b
magallanesgabo <96898684+magallanesgabo@users.noreply.github.com>
2022-08-04 23:35:08 -0400

    QR 2

--- src/components/componentsVendor/QRscan.js
+++ src/components/componentsVendor/QRscan.js
@@ -25,21 +25,17 @@ const QRscan = (props) => {
           },
         }).then(function (response) {
           console.log(response);
-          if(response.statusText === 'Created'){
-           window.location.href = `/vendor/success`;
-          }if(response.data.code === 400){
-            window.location.href = `/vendor/error`;
+          if(response.status === 201){
+            window.location.href = `/vendor/success`;
           }if(response.data.code === 404){
             window.location.href = `/vendor/invalid`;
+          }if(response.data.code === 400){
+            window.location.href = `/vendor/error`;
           }
         })
         .catch(function (response) {
           console.log(response);
-          if(response.data.code === 404){
-            window.location.href = `/vendor/invalid`;
-          }if(response.data.code === 400){
-            window.location.href = `/vendor/error`;
-          }if(response.data.code === 500){
+          if(response.data.code === 500){
             window.location.href = `/vendor/out`;
           }
         });
